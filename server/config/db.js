24c116adb76d3affe53c2ca257a193e9969e3ed2
deveshcse <d.mishra56603@gmail.com>
2026-03-13 23:53:24 +0530

    fix: simplified db connection

--- server/config/db.js
+++ server/config/db.js
@@ -2,8 +2,9 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`);
+    const connectionInstance = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+    console.log(` Connected to database: ${connectionInstance.connection.name}`);
   } catch (error) {
     console.log("MONGODB connection FAILED ", error);
     process.exit(1);
